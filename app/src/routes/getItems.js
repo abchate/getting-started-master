8861352c5ece75b4c0b448a53da096d731c15021
abchate <aliabchate@gmail.com>
2025-05-23 15:07:27 +0200

    getitems

--- app/src/routes/getItems.js
+++ app/src/routes/getItems.js
@@ -2,5 +2,5 @@ const db = require('../persistence');
 
 module.exports = async function (req, res) {
     const items = await db.getItems();
-    res.json({ items }); // réponse modifiée (objet avec clé 'items')
+    res.json(items );
 }
